56e606568b02063e56834c7f319212cae2d0c1d0
AebyZayn <115506566+aebyzayn@users.noreply.github.com>
2024-02-03 19:21:01 +0600

    Add files via upload

---
+++ data/Data.js
@@ -0,0 +1,183 @@
+
+// Header Data.......................
+import homeImage from '../assets/home.png'
+
+export const homeData =[
+    {
+        id: 1,
+        img: homeImage,
+        clip1:'Michael',               
+        clip2:'User Interface',               
+        clip3:'Designer',               
+        info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan.',
+        btnLink: '#Contact'
+    }, 
+];
+
+
+// Brand Data.......................
+import brandImage1 from '../assets/barnd1.png'
+import brandImage2 from '../assets/brand2.png'
+import brandImage3 from '../assets/brand3.png'
+import brandImage4 from '../assets/brand4.png'
+
+export const brandData =[
+    {
+        id: 1,
+        img: brandImage1,
+    },
+    {
+        id: 2,
+        img: brandImage2,
+    },
+    {
+        id: 3,
+        img: brandImage3,
+    },
+    {
+        id: 4,
+        img: brandImage4,
+    },
+]
+
+// AboutData Data.......................
+import aboutImage from '../assets/about.png'
+import CV from '../assets/CV.pdf'
+
+export const aboutData =[
+    {
+        id: 1,
+        img: aboutImage, 
+        aboutInfo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. ' ,        
+        btnLink: CV,    
+    },
+];
+
+
+// Services Data.......................
+import service1 from '../assets/s1.png'
+import service2 from '../assets/s2.png'
+import service3 from '../assets/s3.png'
+import service4 from '../assets/s4.png'
+import service5 from '../assets/s5.png'
+import service6 from '../assets/s6.png'
+
+
+
+
+export const servicesData =[
+    {
+        id: 1,
+        img: service1,
+        serviceName: 'Web Design',
+        serviceInfo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. '        
+    },
+    {
+        id: 2,
+        img: service2, 
+        serviceName: 'Web Devlopment',
+        serviceInfo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. '        
+    },
+    {
+        id: 3,
+        img: service3,    
+        serviceName: 'Marketing',
+        serviceInfo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. '     
+    },   
+    {
+        id: 4,
+        img: service4,    
+        serviceName: 'Photograpy',
+        serviceInfo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. '     
+    },   
+    {
+        id: 5,
+        img: service5,    
+        serviceName: 'Copy Paste',
+        serviceInfo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. '     
+    },   
+    {
+        id: 6,
+        img: service6,    
+        serviceName: 'Organizing',
+        serviceInfo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. '     
+    },   
+];
+
+
+// Portfolio Data.......................
+import portfolioImg1 from '../assets/p1.png'
+import portfolioImg2 from '../assets/p2.png'
+import portfolioImg3 from '../assets/p3.png'
+import portfolioImg4 from '../assets/p4.png'
+import portfolioImg5 from '../assets/p5.png'
+import portfolioImg6 from '../assets/p6.png'
+
+
+
+export const portfolioData =[
+    {
+        id: 1,
+        img: portfolioImg1,
+        name: 'App Dashboard',
+        info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ',        
+        btnLink: '#'
+    },
+    {
+        id: 2,
+        img: portfolioImg2,
+        name: 'App Dashboard',
+        info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse',        
+        btnLink: '#'
+    },
+    {
+        id: 3,
+        img: portfolioImg3,
+        name: 'Easy Rent',
+        info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ',        
+        btnLink: '#'
+    },
+    {
+        id: 4,
+        img: portfolioImg4,
+        name: 'Easy Rent',
+        info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse',        
+        btnLink: '#'
+    },  
+    {
+        id: 5,
+        img: portfolioImg5,
+        name: 'Easy Rent',
+        info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse',        
+        btnLink: '#'
+    },  
+    {
+        id: 6,
+        img: portfolioImg6,
+        name: 'Easy Rent',
+        info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse',        
+        btnLink: '#'
+    },  
+];
+
+
+
+// HappyClinte Data.......................
+export const happyclientData=[
+    {
+        id: 1,
+        topic: '15 National',        
+        info: 'Awards',        
+    },   
+    {
+        id: 2,
+        topic: '1500+ Happy',        
+        info: 'Customers',        
+    },   
+    {
+        id: 3,
+        topic: '2700+ Lovely',        
+        info: 'Feedbacks',        
+    },   
+];
+
